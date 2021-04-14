3f0655fef06f5cfa99d5684f911b68157f9fd53f
vladislav_veryasov <verasoww@gmail.com>
2021-04-14 15:02:45 +0300

    init

---
+++ src/test/java/ru/edu/iorder/preorder/PreOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.iorder.preorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
