4e3f0b0f3321548bd4637aa1350a74c9795d19ea
vladislav_veryasov <verasoww@gmail.com>
2021-05-06 19:05:47 +0300

    add cloud

--- auth/src/main/java/ru/edu/iorder/auth/repository/RoleRepository.java
+++ auth/src/main/java/ru/edu/iorder/auth/repository/RoleRepository.java
@@ -1,8 +1,10 @@
 package ru.edu.iorder.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.edu.iorder.auth.model.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
--- auth/src/main/java/ru/edu/iorder/auth/repository/UserRepository.java
+++ auth/src/main/java/ru/edu/iorder/auth/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package ru.edu.iorder.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.edu.iorder.auth.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserName(String name);
 }
