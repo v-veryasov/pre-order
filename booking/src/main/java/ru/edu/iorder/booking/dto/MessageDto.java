1a1e30cfd0035e2b2a2c553e6cda12c5789df759
vladislav_veryasov <verasoww@gmail.com>
2021-05-07 18:03:33 +0300

    add kafka

---
+++ booking/src/main/java/ru/edu/iorder/booking/dto/MessageDto.java
@@ -0,0 +1,20 @@
+package ru.edu.iorder.booking.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDto {
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("identifier")
+    private int identifier;
+}
