92ca78203f1f12c6f23e2dc0636ede738d315db3
vladislav_veryasov <verasoww@gmail.com>
2021-05-14 16:12:06 +0300

    add structure sso auth

---
+++ auth-sso/src/main/java/ru/edu/iorder/sso/auth/Application.java
@@ -0,0 +1,19 @@
+package ru.edu.iorder.sso.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
