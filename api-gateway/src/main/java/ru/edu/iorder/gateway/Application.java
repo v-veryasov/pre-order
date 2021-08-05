9a41232044861d4b4aa9208a1e2e319d0a9a38db
vladislav_veryasov <verasoww@gmail.com>
2021-08-05 14:39:36 +0300

    Disable zuul and add Cloud Gateway (exist problem: get id pod instead of service id)

--- api-gateway/src/main/java/ru/edu/iorder/gateway/Application.java
+++ api-gateway/src/main/java/ru/edu/iorder/gateway/Application.java
@@ -2,16 +2,19 @@ package ru.edu.iorder.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
-@EnableCircuitBreaker
-@EnableZuulProxy
+@EnableEurekaClient
+//@EnableFeignClients
+//@EnableCircuitBreaker
+@EnableHystrix
+//@EnableZuulProxy
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
